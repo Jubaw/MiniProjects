f06571f47f5decf6736b5edb6e06653b190640ad
Jubaw <kaliferno@gmail.com>
2024-03-13 22:16:17 +0300

    -Added listBooks method

--- src/LibraryBookManagementSystem/BookMenu.java
+++ src/LibraryBookManagementSystem/BookMenu.java
@@ -1,7 +1,10 @@
 package LibraryBookManagementSystem;
 
+import javax.xml.crypto.Data;
 import java.awt.print.Book;
 import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class BookMenu {
@@ -20,7 +23,9 @@ public class BookMenu {
                     1.Find book by author name\s
                     2.Find author by book name
                     3.Find book by genre
-                    4.Exit""");
+                    4.Add Book
+                    5.List Books
+                    6.Exit""");
             menuSelection = scanner.nextLine();
             switch (menuSelection) {
                 case "1":
@@ -33,15 +38,21 @@ public class BookMenu {
                     findBookByGenre();
                     break;
                 case "4":
+                    addBook();
+                    break;
+                case "5":
+                    listBooks();
+                    break;
+                case "6":
                     exit();
                     return;
                 default:
-                    System.out.println("Invalid selection, choose from 1 to 4");
+                    System.out.println("Invalid selection, choose from 1 to 6");
 
             }
 
 
-        } while (!menuSelection.equalsIgnoreCase("4"));
+        } while (!menuSelection.equalsIgnoreCase("6"));
     }
 
 
@@ -130,4 +141,52 @@ public class BookMenu {
         System.out.println("Exiting program...");
         System.exit(0);
     }
+
+
+    public void addBook(){
+        System.out.println("Bookname: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Book author: ");
+        String author = scanner.nextLine();
+
+        System.out.println("Book price: ");
+        int price = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Genre: ");
+        String genre = scanner.nextLine();
+
+        Books newBook = new Books(name,author,price,genre);
+        database.addBook(newBook);
+
+        System.out.println(STR."Book added\nName:\{name}\nAuthor: \{author}\nPrice: \{price}\nGenre: \{genre}");
+
+        System.out.println("Add new book ? Y/N");
+        String input = scanner.nextLine();
+        if (input.equalsIgnoreCase("Y")){
+            addBook();
+        }
+
+    }
+
+
+    public void listBooks(){
+        List<Books> books = database.getBooksList();
+        if (books.isEmpty()){
+            System.out.println("There are no books in library");
+        }else{
+            for (Books book : database.getBooksList()){
+                System.out.println("======================");
+                System.out.println("Book Name: " + book.getBookName() +
+                        "\nAuthor: " + book.getBookAuthor() +
+                        "\nPrice: $" + book.getBookPrice() +
+                        "\nGenre: " + book.getBookGenre());
+            }
+            System.out.println("======================");
+        }
+
+    }
+
+
 }
