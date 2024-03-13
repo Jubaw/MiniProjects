22bbffacce84edc9acf07956a957fc2d61f20702
Jubaw <kaliferno@gmail.com>
2024-03-13 22:17:35 +0300

    No-Changes

--- src/LibraryBookManagementSystem/LibraryRunner.java
+++ src/LibraryBookManagementSystem/LibraryRunner.java
@@ -1,17 +1,12 @@
 package LibraryBookManagementSystem;
 
+import javax.swing.*;
+
 public class LibraryRunner {
     public static void main(String[] args) {
         BookMenu bookMenu = new BookMenu();
+        Database database = new Database(); // Make sure this matches your actual database setup
 
-
-
-bookMenu.startMenu();
-
-
-   //    bookMenu.findBookByAuthor();
-        //     bookMenu.findAuthorByBook();
-//          bookMenu.findBookByGenre();
-
+        bookMenu.startMenu();
     }
 }
